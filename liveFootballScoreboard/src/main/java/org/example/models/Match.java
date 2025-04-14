0a148260935200bba818dca21d683a2650b2a3b5
Ivana Kovacevic <kovacevic.ra15.2020@uns.ac.rs>
2025-04-14 13:33:16 +0200

    feat: Add Match model to the project

---
+++ liveFootballScoreboard/src/main/java/org/example/models/Match.java
@@ -0,0 +1,51 @@
+package org.example.models;
+
+public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private int homeTeamScore;
+    private int awayTeamScore;
+    private long startTime;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeTeamScore = 0;
+        this.awayTeamScore = 0;
+        this.startTime = System.nanoTime();
+    }
+
+    public int getHomeScore() {
+        return homeTeamScore;
+    }
+
+    public int getAwayScore() {
+        return awayTeamScore;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void updateScore(int home, int away){
+        this.homeTeamScore=home;
+        this.awayTeamScore=away;
+    }
+
+    public int getTotalScore(){
+        return this.homeTeamScore+this.awayTeamScore;
+    }
+
+    @Override
+    public String toString() {
+        return homeTeam+" "+homeTeamScore+" - "+awayTeam+" "+awayTeamScore;
+    }
+}
