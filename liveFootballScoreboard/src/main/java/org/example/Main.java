fd0c03f56485f88d3cf0eeb0f45e5b8aa3a0cc55
Ivana Kovacevic <kovacevic.ra15.2020@uns.ac.rs>
2025-04-15 12:38:36 +0200

    chore: Add configuration for building jar library

--- liveFootballScoreboard/src/main/java/org/example/Main.java
+++ liveFootballScoreboard/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.iservices.IScoreboardService;
+import org.example.services.ScoreboardService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
