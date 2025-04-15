9b9aebee698507ab5ad5869265ad7ee3da9bf36e
Ivana Kovacevic <kovacevic.ra15.2020@uns.ac.rs>
2025-04-15 12:43:23 +0200

    test: Add initial test data for scoreboard in main

--- liveFootballScoreboard/src/main/java/org/example/Main.java
+++ liveFootballScoreboard/src/main/java/org/example/Main.java
@@ -5,6 +5,26 @@ import org.example.services.ScoreboardService;
 
 public class Main {
     public static void main(String[] args) {
+        IScoreboardService scoreboardService=new ScoreboardService();
+        //start matches
+        scoreboardService.startMatch("Mexico", "Canada");
+        scoreboardService.startMatch("Spain", "Brazil");
+        scoreboardService.startMatch("Germany", "France");
+        scoreboardService.startMatch("Uruguay", "Italy");
+        scoreboardService.startMatch("Argentina", "Australia");
+
+        //update scores
+        scoreboardService.updateScore("Mexico", "Canada",0,5);
+        scoreboardService.updateScore("Spain", "Brazil",10,2);
+        scoreboardService.updateScore("Germany", "France",2,2);
+        scoreboardService.updateScore("Uruguay", "Italy",6,6);
+        scoreboardService.updateScore("Argentina", "Australia",3,1);
+
+        //reviews
+        int i=1;
+        for(int j=0;j<scoreboardService.getAll().size();j++){
+            System.out.println(i++ + ". "+scoreboardService.getAll().get(j));
+        }
 
     }
 }
